b3534a495cc9730e3a467dc99a9bb8115fc2da7b
GChukwudi <g.chukwudi@alustudent.com>
2023-11-01 15:06:36 -0700

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
             .format(employee_id))
 
     user_info = requests.get(url).json()
-    todo_info = requests.get(todo).json()
-    completed_tasks = [task['title'] for task in todo_info if task['completed']]
+    todo_i = requests.get(todo).json()
+    completed_tasks = [task['title'] for task in todo_i if task['completed']]
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(user_info['name'], len(completed_tasks), len(todo_info)))
+          .format(user_info['name'], len(completed_tasks), len(todo_i)))
     print("\n".join(["\t{}".format(task) for task in completed_tasks]))
