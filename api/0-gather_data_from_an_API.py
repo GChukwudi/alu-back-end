f62898034303a629480aaaf169b24fb488321b9b
GChukwudi <g.chukwudi@alustudent.com>
2023-11-01 14:24:40 -0700

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,8 +23,5 @@ if __name__ == "__main__":
     task_com = len(total_tasks)
     total_task_done = len(todo_info)
 
-    message = (
-        f"Employee {employee_name} is done({task_com}/{total_task_done}):"
-    )
-    print(message)
+    print(f"Employee {employee_name} is done({task_com}/{total_task_done}):")
     [print(f"\t{task.get('title')}") for task in total_tasks]
