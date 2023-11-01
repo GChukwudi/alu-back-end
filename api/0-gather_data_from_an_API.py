66a66bc17a5cb7823a5ab80a69d92f25871fb622
GChukwudi <g.chukwudi@alustudent.com>
2023-11-01 14:54:08 -0700

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,15 +4,18 @@
 import requests
 from sys import argv
 
-""" Script to  return a given employee ID
-together with his/her TODO list progress
+""" Script to return a given employee ID
+together with their TODO list progress
 """
 
 if __name__ == "__main__":
     employee_id = argv[1]
-    url = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}", verify=False).json()
-    todo = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}", verify=False).json()
-    completed_tasks = [task['title'] for task in todo if task.get('completed')]
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
 
-    print(f"Employee {url.get('name')} is done with tasks({len(completed_tasks)}/{len(todo)}):")
+    user_info = requests.get(url).json()
+    todo_info = requests.get(todo).json()
+    completed_tasks = [task['title'] for task in todo_info if task['completed']]
+
+    print(f"Employee {user_info['name']} is done with tasks({len(completed_tasks)}/{len(todo_info)}):")
     print("\n".join([f"\t{task}" for task in completed_tasks]))
