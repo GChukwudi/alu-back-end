4e6cdab53383ffd22a36809ea1c53627350664f9
GChukwudi <g.chukwudi@alustudent.com>
2023-11-01 14:00:59 -0700

    Gather data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+""" Script to  return a given employee ID
+together with his/her TODO list progress
+"""
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+
+    todo = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todo = todo.format(employee_id)
+
+    user_info = requests.request("GET", url).json()
+    todo_info = requests.request("GET", todo).json()
+
+    employee_name = user_info.get("name")
+    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
+    task_com = len(total_tasks)
+    total_task_done = len(todo_info)
+
+    print(f"Employee {employee_name} is done with tasks({task_com}/{total_task_done}):")
+    [print(f"\t{task.get('title')}") for task in total_tasks]
